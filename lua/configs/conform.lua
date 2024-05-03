8c4f2dbdecf08ee176891cda017553fbee84ee3c
kartikkanugo <karthik.kanugo@gmail.com>
2024-05-03 10:20:06 +0530

    added some minor lsp config

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,5 +1,9 @@
 local options = {
-	lsp_fallback = true,
+	format_on_save = {
+		-- These options will be passed to conform.format()
+		timeout_ms = 500,
+		lsp_fallback = true,
+	  },
 
 
 	formatters_by_ft = {
